f30e4a38daa3df6b20cc98f749d7bc3178f344aa
Dhananjay Puranik <dhananjaypuranik@Dhananjays-MacBook-Pro.local>
2024-03-01 01:04:03 +0530

    project changes

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -13,81 +13,81 @@ const data = [
         id: 1,
         category: 'Html/Css',
         image: Image1,
-        title: "Project Title One (Html/Css)",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-        demo: 'http://egatortutorials.com',
-        github: 'https://github.com/egattor'
+        title: "Spotify Landing Page (Html/Css)",
+        desc: "A Responsive Landing Page of Music Player Spotify using Html and Css.",
+        demo: 'https://spoify-land-page.netlify.app',
+        github: 'https://github.com/dhananjayp8/Spoify-Landing-Page'
     },
     {
         id: 2,
         category: 'Html/Css',
         image: Image2,
-        title: "Project Title Two (Html/Css)",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-        demo: 'http://egatortutorials.com',
-        github: 'https://github.com/egattor'
+        title: "Zomato Clone (Html/Css)",
+        desc: "A basic replica of  zomato landing page clone",
+        demo: 'https://zomato-website-landing-clone.netlify.app/',
+        github: 'https://github.com/dhananjayp8/zomato-website-clone'
     },
     {
         id: 3,
         category: 'React',
         image: Image3,
-        title: "Project Title Three (React)",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-        demo: 'http://egatortutorials.com',
-        github: 'https://github.com/egattor'
+        title: "Netflix Clone (React)",
+        desc: "Created a clone of Netflix clone having two pages. Used Firebase Authentication for user registration.Used TMDb Movie Api.!",
+        demo: 'https://netflix-landwebsite-clone.netlify.app/',
+        github: 'https://github.com/dhananjayp8/Netflix-Clone'
     },
     {
         id: 4,
         category: 'Html/Css',
         image: Image4,
-        title: "Project Title Four (Html/Css)",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-        demo: 'http://egatortutorials.com',
-        github: 'https://github.com/egattor'
+        title: "Login Page (Html/Css)",
+        desc: "Created a generic Social Media Login page Website.",
+        demo: 'https://socialmedia-login-page.netlify.app',
+        github: 'https://github.com/dhananjayp8/Loginpage'
     },
     {
         id: 5,
         category: 'Html/Css',
         image: Image5,
-        title: "Project Title Five (Html/Css)",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! ",
-        demo: 'http://egatortutorials.com',
-        github: 'https://github.com/egattor'
+        title: "Facebook Login (Html/Css)",
+        desc: "Created a replica of Facebook Landing login page.",
+        demo: 'https://facebookwebsite-login-page.netlify.app/',
+        github: 'https://github.com/dhananjayp8/Facebook-Log-in'
     },
     {
         id: 6,
         category: 'React',
         image: Image6,
-        title: "Project Title Six (React)",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-        demo: 'http://egatortutorials.com',
-        github: 'https://github.com/egattor'
+        title: "TaskMate (React)",
+        desc: "Created a taskmate manager to add,delete and update tasks for record.",
+        demo: 'https://task-mate-react-page.netlify.app/',
+        github: 'https://github.com/dhananjayp8/Taskmate'
     },
     {
         id: 7,
         category: 'Javascript',
         image: Image7,
-        title: "Project Title Seven (Javascript)",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-        demo: 'http://egatortutorials.com',
-        github: 'https://github.com/egattor'
+        title: "Form Validation (Javascript)",
+        desc: "Form Validation where every input field is validated using Javascript",
+        demo: 'https://socialmedia-form-validation-page.netlify.app',
+        github: 'https://github.com/dhananjayp8/social-form-validation'
     },
     {
         id: 8,
         category: 'Javascript',
         image: Image8,
-        title: "Project Title Eight (Javascript)",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-        demo: 'http://egatortutorials.com',
-        github: 'https://github.com/egattor'
+        title: "Tic Tac Toe (Javascript)",
+        desc: "Tic-tac-toe, noughts and crosses, or Xs and Os is a paper-and-pencil game for two players who take turns marking the spaces in a three-by-three grid with X or O.",
+        demo: 'https://tic-tac-toe-gamepage.netlify.app',
+        github: 'https://github.com/dhananjayp8/Tic-Tac-Toe'
     },
     {
         id: 9,
         category: 'Javascript',
         image: Image9,
-        title: "Project Title Nine (Javascript)",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-        demo: 'http://egatortutorials.com',
+        title: "2048 Game (Javascript)",
+        desc: "Use your arrow keys or swipe left or right, up or down to move the tiles. When two tiles with the same number touch, they merge into one. Once you get 2048 in any square, you win.",
+        demo: 'https://github.com/dhananjayp8/2048-game',
         github: 'https://github.com/egattor'
     }
 ]
