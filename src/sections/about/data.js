b7472d8c6a8648b6c634356f3d33468f522a40b7
Dhananjay Puranik <dhananjaypuranik@Dhananjays-MacBook-Pro.local>
2024-03-01 20:28:25 +0530

    changes in skills

--- src/sections/about/data.js
+++ src/sections/about/data.js
@@ -2,7 +2,7 @@ import {FaAward} from 'react-icons/fa'
 import {TbBooks} from 'react-icons/tb'
 
 const data = [
-    {id: 1, icon: <FaAward/>, title: 'Certifications'},
+    {id: 1, icon: <FaAward/>, title: 'Skills'},
     {id: 2, icon: <TbBooks/>, title: 'Projects'},
     ]
 
